8c5eba44e1bf254aad8e362aa4cc2dcf14f7918e
sensei <sensei@topo.dhcp.fnal.gov>
2020-08-14 15:18:42 -0400

    write starting at the beginning of the flash page

--- writeMCS.py
+++ writeMCS.py
@@ -39,34 +39,42 @@ def recordToString(record):
     offset = record[2]
     dataLen = len(record[1])
     dataString = ""
-    checksum = dataLen + offset + recordType
-    #NOTE: checksum calculation is incorrect for offsets > 0xFF, but this is never the case in this script
+    checksum = dataLen + (offset % 256) + (offset / 256) + recordType
     for dataByte in dataList:
         dataString += ("%02X" % ord(dataByte))
         checksum += ord(dataByte)
     checksum %= 256
-    checksum = 256-checksum
+    if checksum != 0:
+        checksum = 256-checksum
     return ":{0:02X}{1:04X}{2:02X}{3:s}{4:02X}\n".format(dataLen, offset, recordType, dataString, checksum)
 
 
 with open(mcsFilename, 'w') as mcsFile:
     mcsFile.write(recordToString(extendedLinearAddress(0x03FF)))
 
+    currentAddr = 0x00
+    lastAddr = 0xFEFF
+    while lastAddr>currentAddr:
+        nextAddr = min(lastAddr, currentAddr+0xF)+1
+        d = ['\xFF' for i in range(currentAddr, nextAddr)]
+        mcsFile.write(recordToString((0x00, d, currentAddr)))
+        currentAddr = nextAddr
+
     d = struct.pack('BBHBBHII', info["Firmware"]["version"]["minor"], info["Firmware"]["version"]["major"], 0xFFFF, info["Firmware"]["date"]["month"], info["Firmware"]["date"]["day"], info["Firmware"]["date"]["year"], int(info["Firmware"]["hash"][:8],16), int(info["Firmware"]["hash"][8:],16))
-    mcsFile.write(recordToString((0x00, d, 0x00)))
+    mcsFile.write(recordToString((0x00, d, 0xFF00)))
 
     d = struct.pack('BBHBBHII', info["Software"]["version"]["minor"], info["Software"]["version"]["major"], 0xFFFF, info["Software"]["date"]["month"], info["Software"]["date"]["day"], info["Software"]["date"]["year"], int(info["Software"]["hash"][:8],16), int(info["Software"]["hash"][8:],16))
-    mcsFile.write(recordToString((0x00, d, 0x10)))
+    mcsFile.write(recordToString((0x00, d, 0xFF10)))
 
     ip_dec = 0
     for word in info["Board IP"].split("."):
         ip_dec <<= 8
         ip_dec += int(word)
     d = struct.pack('IIQ', int(info["Unique ID"]+id_num[2:],16), ip_dec, 0xFFFFFFFFFFFFFFFF)
-    mcsFile.write(recordToString((0x00, d, 0x20)))
+    mcsFile.write(recordToString((0x00, d, 0xFF20)))
 
-    currentAddr = 0x30
-    lastAddr = 0xFE
+    currentAddr = 0xFF30
+    lastAddr = 0xFFFF
     while lastAddr>currentAddr:
         nextAddr = min(lastAddr, currentAddr+0xF)+1
         d = ['\xFF' for i in range(currentAddr, nextAddr)]
