2d9472eeae45e9b0b48193ef67d6828d6d310e03
SENSEI <sensei@pazu>
2021-07-01 14:33:22 +0300

    script tweaks

--- vivado/write_fw.sh
+++ vivado/write_fw.sh
@@ -1,19 +1,21 @@
-#source this script from a directory that contains memory.mcs (or memory.mcs.gz) and memory.prm, e.g.:
+#!/bin/bash
+
+#run this script from a directory that contains memory.mcs (or memory.mcs.gz) and memory.prm, e.g.:
 #  cd fw/v24
-#  source ../../vivado/write_fw.sh
+#  ../../vivado/write_fw.sh
 #this will write the FW on all connected LTAs
 
 #get the path to ltaFlash/vivado
-scriptdir=$(dirname "$BASH_SOURCE")
+scriptdir=$(dirname "$0")
 
 #find the correct vivado command
 if command -v vivado_lab &> /dev/null; then
     vivadoname="vivado_lab"
-else if command -v vivado &> /dev/null; then
+elif command -v vivado &> /dev/null; then
     vivadoname="vivado"
 else
-    vivadoname=""
-fi
+    echo "no vivado command in environment; do you need to source settings64.sh?"
+    exit
 fi
 
 #if we have no .mcs file but we have the .mcs.gz
@@ -22,8 +24,4 @@ if [ ! -e "memory.mcs" ] && [ -e "memory.mcs.gz" ]; then
     gunzip -k memory.mcs.gz
 fi
 
-if [ "$vivadoname" ]; then
-    $vivadoname -mode batch -notrace -source $scriptdir/write_flash.tcl
-else
-    echo "no vivado command in environment; do you need to source settings64.sh?"
-fi
+$vivadoname -mode batch -notrace -source "$scriptdir"/write_flash.tcl
