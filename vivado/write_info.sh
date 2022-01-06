c65de7c78f857607aeb270041dedbb1fa6769258
sensei development <sensei@totoro.dhcp.fnal.gov>
2022-01-06 09:17:58 -0600

    write_info.sh can take FPGA DNA argument

--- vivado/write_info.sh
+++ vivado/write_info.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #run this script from a directory that contains flashInfo.json
-#need 1 or 2 arguments for LTA number and (optionally) IP, e.g.:
+#need 1, 2, or 3 arguments for LTA number and (optionally) IP and FPGA DNA, e.g.:
 #  ../../vivado/write_info.sh 12
 #  ../../vivado/write_info.sh 12 192.168.133.3
-#this will write the flash info on all connected (USB-JTAG plugged in, and powered) LTAs, so you should only use this script with a single LTA connected at a time
-#if you have multiple LTAs connected you can write to individual ones using the FPGA DNA, see write_flash.tcl
+#  ../../vivado/write_info.sh 12 192.168.133.3 2A12111E7221002B
+# the first 2 forms will write the flash info on all connected (USB-JTAG plugged in, and powered) LTAs
+# if you have multiple LTAs connected you must use the last form to specify which FPGA DNA you are configuring
 
 #get the path to ltaFlash/vivado
 scriptdir=$(dirname "$0")
@@ -23,5 +24,16 @@ fi
 #delete the existing memory_info.mcs, so we don't accidentally write a stale one
 rm -f memory_info.mcs memory_info.prm
 ln -s "$scriptdir"/memory_info.prm .
-"$scriptdir"/writeMCS.py flashInfo.json "$@"
-$vivadoname -mode batch -notrace -source "$scriptdir"/write_flash.tcl -tclargs memory_info
+if [[ "$#" -eq 1 ]]
+then
+    "$scriptdir"/writeMCS.py flashInfo.json "$1"
+else
+    "$scriptdir"/writeMCS.py flashInfo.json "$1" "$2"
+fi
+
+if [[ "$#" -eq 3 ]]
+then
+    $vivadoname -mode batch -notrace -source "$scriptdir"/write_flash.tcl -tclargs memory_info "$3"
+else
+    $vivadoname -mode batch -notrace -source "$scriptdir"/write_flash.tcl -tclargs memory_info
+fi
